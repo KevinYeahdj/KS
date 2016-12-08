e2b6bfc6b47f89299613265b8d913e48c55407e4
Kevin <yeahdj@163.com>
2016-12-09 02:14:42 +0800

    1

--- HRMS/Controllers/ManageController.cs
+++ HRMS/Controllers/ManageController.cs
@@ -35,6 +35,11 @@ namespace HRMS.Controllers
 
             return View();
         }
+
+        public ActionResult UsersIndex()
+        {
+            return View();
+        }
         public ActionResult DicIndex()
         {
             DicManager dm = new DicManager();
@@ -74,6 +79,85 @@ namespace HRMS.Controllers
     {
 
         #region 用户方法
+        public void GetAllUsers_old()
+        {
+            //用于序列化实体类的对象  
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+
+            //请求中携带的条件  
+            string order = HttpContext.Request.Params["order"];
+            string sort = HttpContext.Request.Params["sort"];
+            string searchKey = HttpContext.Request.Params["search"];
+            int offset = Convert.ToInt32(HttpContext.Request.Params["offset"]);  //0
+            int pageSize = Convert.ToInt32(HttpContext.Request.Params["limit"]);
+
+            int total = 0;
+            UserManager um = new UserManager();
+            List<UserEntity> list = um.GetSearch(searchKey, sort, order, offset, pageSize, out total);
+
+            DicManager dm = new DicManager();
+            List<DicEntity> companyDicE = dm.GetDicByType("公司");
+            Dictionary<string, string> companyDic = new Dictionary<string, string>();
+            foreach (var item in companyDicE)
+            {
+                companyDic.Add(item.iKey, item.iValue);
+            }
+            List<UserViewModel> listView = new List<UserViewModel>();
+            foreach (var item in list)
+            {
+                listView.Add(new UserViewModel { iCompanyCode = item.iCompanyCode, iCompanyName = item.iCompanyCode, iEmployeeCodeId = item.iEmployeeCodeId, iPassWord = item.iPassWord, iUserName = item.iUserName, iUserType = item.iUserType, iUpdatedOn = item.iUpdatedOn.ToString("yyyyMMdd HH:mm") });
+            }
+
+            //给分页实体赋值  
+            PageModels<UserViewModel> model = new PageModels<UserViewModel>();
+            model.total = total;
+            if (total % pageSize == 0)
+                model.page = total / pageSize;
+            else
+                model.page = (total / pageSize) + 1;
+
+            model.rows = listView;
+
+            //将查询结果返回  
+            HttpContext.Response.Write(jss.Serialize(model));
+        }
+
+        public string UsersSaveChanges_old(string jsonString, string action)
+        {
+
+            try
+            {
+                UserEntity ue = JsonConvert.DeserializeObject<UserEntity>(jsonString);
+                UserManager pm = new UserManager();
+                if (action == "add")
+                {
+                    ue.iCreatedBy = SessionHelper.CurrentUser.UserName;
+                    ue.iUpdatedBy = SessionHelper.CurrentUser.UserName;
+                    pm.Insert(ue);
+                }
+                else
+                {
+                    UserEntity ueOld = pm.GetUser(ue.iEmployeeCodeId);
+                    ue.iUpdatedBy = SessionHelper.CurrentUser.UserName;
+                    ue.iCreatedBy = ueOld.iCreatedBy;
+                    ue.iCreatedOn = ueOld.iCreatedOn;
+                    if (ue.iIsDeleted != 1)
+                    {
+                        ue.iStatus = 1;
+                    }
+                    pm.Update(ue);
+                }
+                return "success";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
+        #endregion
+
+        #region 新用户方法
         public void GetAllUsers()
         {
             //用于序列化实体类的对象  
@@ -117,7 +201,7 @@ namespace HRMS.Controllers
             HttpContext.Response.Write(jss.Serialize(model));
         }
 
-        public string UsersSaveChanges(string jsonString, string action)
+        public string UserSaveChanges(string jsonString, string action)
         {
 
             try
@@ -148,6 +232,11 @@ namespace HRMS.Controllers
             {
                 return e.ToString();
             }
+        }
+
+        public string CheckUserValid(string iEmployeeCodeId)
+        {
+            return "valid";
         }
 
         #endregion
