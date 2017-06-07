6dae8857a88078f280e5f3be793c3e24395a3a3b
Kevin <yeahdj@163.com>
2017-06-07 18:17:38 +0800

    1

--- GenerateDataMonthly/Program.cs
+++ GenerateDataMonthly/Program.cs
@@ -13,21 +13,27 @@ namespace GenerateDataMonthly
         static void Main(string[] args)
         {
             //社保分地区生成，公积金以上海为准
-            //上海 每月最后一天11点生成
+            //上海 每月最后一天11点生成--改成次月7号
             //苏州 每月9号晚11点生成
             //无锡 每月25号晚11点生成
             if (args.Length == 0)
                 return;
             int paidMonth = int.Parse(DateTime.Now.ToString("yyyyMM"));
             string iPayPlace = args[0];
-            SocialSecurityManager serviceSocialSecurity = new SocialSecurityManager();
-            int count = serviceSocialSecurity.GenerateSocialSecurityDetailMonthly(paidMonth, iPayPlace);
-            LogFileHelper.WriteLog("社保【" + paidMonth.ToString() + "】生成成功，生成条数【" + count.ToString() + "】");
-
-            if (iPayPlace == "上海")
+            if (iPayPlace == "上海")   //上海生成上月
+            {
+                paidMonth = int.Parse(DateTime.Now.AddMonths(-1).ToString("yyyyMM"));
+            }
+            if (!string.IsNullOrEmpty(iPayPlace))  //非空生成社保
+            {
+                SocialSecurityManager serviceSocialSecurity = new SocialSecurityManager();
+                int count = serviceSocialSecurity.GenerateSocialSecurityDetailMonthly(paidMonth, iPayPlace);
+                LogFileHelper.WriteLog("社保【" + paidMonth.ToString() + "】生成成功，生成条数【" + count.ToString() + "】");
+            }
+            else  //空生成公积金
             {
                 ProvidentFundManager serviceProvidentFund = new ProvidentFundManager();
-                count = serviceProvidentFund.GenerateProvidentFundDetailMonthly(paidMonth);
+                int count = serviceProvidentFund.GenerateProvidentFundDetailMonthly(paidMonth);
                 LogFileHelper.WriteLog("公积金【" + paidMonth.ToString() + "】生成成功，生成条数【" + count.ToString() + "】");
 
                 //每月生成每个公司每个项目的 财务汇总记录--让他们手动生成
